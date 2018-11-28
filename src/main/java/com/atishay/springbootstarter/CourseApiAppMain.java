7d6d241adc46944c07037631ba43ed843c37dc6a
Atishay Jain <atishayjain23@gmail.com>
2018-11-28 09:26:46 +0530

    Promoting pom and other minor changes

--- src/main/java/com/atishay/springbootstarter/CourseApiAppMain.java
+++ src/main/java/com/atishay/springbootstarter/CourseApiAppMain.java
@@ -1,8 +1,10 @@
 package com.atishay.springbootstarter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /*
@@ -10,14 +12,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 
 @SpringBootApplication
-//To enable JPA Repository, however it will work if you didn't declare these
-//annotations, reason: Becoz this class is on top of other classes and it will automatically
-//scan the base classes and packages.
+// To enable JPA Repository, however it will work if you didn't declare these
+// annotations, reason: Becoz this class is on top of other classes and it will
+// automatically scan the base classes and packages.
 @EnableJpaRepositories(basePackages = { "com.atishay.springbootstarter.repository" })
-@EntityScan(basePackages= {"com.atishay.springbootstarter.entity"})
+@EntityScan(basePackages = { "com.atishay.springbootstarter.entity" })
+@PropertySource("check.properties")
 public class CourseApiAppMain {
+
+	@Value("${check.properties}")
+	static String dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiAppMain.class, args);
+		System.out.println("***************" + dataSource);
 	}
 }
