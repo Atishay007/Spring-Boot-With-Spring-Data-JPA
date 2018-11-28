4fe7420ef4ff490a2b8f64bc3fd065406ca6cc04
Atishay Jain <atishayjain23@gmail.com>
2018-11-28 13:14:02 +0530

    New TestCases

--- src/test/java/com/springboot/test/TopicServiceTest.java
+++ src/test/java/com/springboot/test/TopicServiceTest.java
@@ -1,17 +1,15 @@
 package com.springboot.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.atLeastOnce;
-
+import java.util.List;
 import java.util.Optional;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import com.atishay.springbootstarter.entity.TopicDTO;
 import com.atishay.springbootstarter.repository.ServiceRepository;
 import com.atishay.springbootstarter.services.TopicService;
@@ -55,12 +53,20 @@ public class TopicServiceTest {
 		System.out.println("@After");
 	}
 
-	//This will skip test case.
-	@Ignore
+	/*
+	 * //This will skip test case.
+	 * 
+	 * @Ignore
+	 */
 	@Test
 	public void testMath() {
 
-		Mockito.when(mockServiceRepo.findById(10l)).thenReturn(topicDto);
+		Mockito.when(mockServiceRepo.findById(10l)).then((i) -> {
+			if (i.getArguments().length > 0) {
+				return topicDto;
+			}
+			return topicDto;
+		});
 		mockTopicService.getTopicByID(10l);
 		Mockito.verify(mockTopicService, atLeastOnce()).getTopicByID(10l);
 		System.out.println("testMath");
@@ -69,5 +75,16 @@ public class TopicServiceTest {
 	@Test
 	public void testMath2() {
 		System.out.println("testMath2");
+	}
+
+	@Test
+	public void testListClass() {
+		List mockListClass = Mockito.mock(List.class);
+		Mockito.when(mockListClass.get(0)).thenReturn(1).thenReturn(Mockito.anyInt());
+		assertEquals(1, mockListClass.get(0));
+	//	assertEquals(2, mockListClass.get(0));
+
+		//Mockito has many static methods like anyInt(), anyChar() etc.
+		assertEquals(Mockito.anyInt(), mockListClass.get(0));
 	}
 }
